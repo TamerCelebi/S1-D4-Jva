1b5eab78805b3512f1704c2f8a7a97fc355af37b
TamerC <kuluzuk1@outlook.com>
2025-03-11 02:47:02 +0300

    S1-D4 Done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
-package org.example;
+import org.example.Player;
+import org.example.Point;
+import org.example.Weapon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0, 0);
+        System.out.println("distance()= " + point.distance());
+
+        Player player = new Player("John", 120, Weapon.SWORD);
+        System.out.println("Player Health: " + player.healthRemaining());
+        player.loseHealth(30);
+        System.out.println("Player Health after damage: " + player.healthRemaining());
+        player.restoreHealth(50);
+        System.out.println("Player Health after healing: " + player.healthRemaining());
     }
 }
